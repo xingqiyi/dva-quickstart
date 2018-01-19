966160fb391d01ce20cb45ced2f24a78e9a6446e
xingqiyi <448378892@qq.com>
2018-01-19 10:53:46 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import dva from 'dva';
+import './index.css';
+
+// 1. Initialize
+// const app = dva();
+
+const app = dva({
+    initialState: {
+        products: [
+            { name: 'dva', id: 1 },
+            { name: 'ant', id: 2 },
+            { name: 'antd', id: 3 },
+        ],
+    },
+});
+
+
+
+
+
+// 2. Plugins
+// app.use({});
+
+// 3. Model
+// app.model(require('./models/example').default);
+app.model(require('./models/products').default)
+
+// 4. Router
+app.router(require('./router').default);
+
+// 5. Start
+app.start('#root');
